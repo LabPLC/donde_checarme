b250944c3b7e6629fdbfd20ab6af5ad5fe1f2d6f
Juan Carlos <juanjcsr@gmail.com>
2014-04-29 16:15:22 -0500

    relacion entre preguntas y lugares, fix #19

--- app/controllers/pregunta_controller.rb
+++ app/controllers/pregunta_controller.rb
@@ -2,9 +2,13 @@ class PreguntaController < ApplicationController
 
   def preguntar
     pregunta = params[:pregunta]
-    a = Pregunta.save!(texto: pregunta)
-
-    lugares = Place.near(a.texto + "")
+    a = Pregunta.create(texto: pregunta)
+    if a.is_in_cat?(a.texto)
+      lugares = Place.get_hospitals
+    else
+      puts "por lugar"
+      lugares = Place.near(a.texto + " Ciudad de Mexico", 2)
+    end
     respond_to do |format|
       format.json do
         to_json lugares
